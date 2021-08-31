af20b7f0917da86a6fac6d7ab8819d5d1b2d57a7
ad <fullmetalbrackets@gmail.com>
2021-08-31 04:19:11 -0400

    fixed link & removed unnecessary JS

--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,7 @@
 </template>
 
 <script>
-export default {
-  head() {
-    return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-    };
-  },
-};
+
 </script>
 
 <style scoped>
