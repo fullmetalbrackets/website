f309c2c35ac91c18414bce8f2b76a19d51fd0497
ad <fullmetalbrackets@gmail.com>
2021-09-01 20:40:06 -0400

    fixing layout & refactoring

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-        <Header />
-        <Navbar />
-        <div class="main">
-            <h1 v-if="error.statusCode === 404" class="err1">Page not found</h1>
-            <h1 v-else class="err1">An error occurred</h1>
-            <p class="err2">Something went wrong. Maybe it's your fault or maybe not. It's all good. <NuxtLink to="/">Return to the home page</NuxtLink>.</p>
-        </div>
-    </div>
+    <main>
+        <h1 v-if="error.statusCode === 404" class="err1">Page not found</h1>
+        <h1 v-else class="err1">An error occurred</h1>
+        <p class="err2">Something went wrong. Maybe it's your fault or maybe not. It's all good. <NuxtLink to="/">Return to the home page</NuxtLink>.</p>
+    </main>
 </template>
 
 <script>
