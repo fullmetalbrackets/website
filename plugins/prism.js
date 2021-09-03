eefb6e2b19a09cd7dcb5a0944067100278cf92c9
ad <fullmetalbrackets@gmail.com>
2021-09-03 03:20:21 -0400

    fixing prism

---
+++ plugins/prism.js
@@ -0,0 +1,14 @@
+import Prism from 'prismjs'
+
+// Include a theme:
+import 'prismjs/themes/prism-okaidia.css'
+
+// Include language support: (optional)
+import 'prismjs/components/prism-scss'
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+import 'prismjs/plugins/line-highlight/prism-line-highlight'
+import 'prismjs/plugins/line-highlight/prism-line-highlight.css'
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
+import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords'
+import 'prismjs/plugins/show-language/prism-show-language'
