9b013c6af593fd5502a2f211eb3a7c78a30f3425
ad <fullmetalbrackets@gmail.com>
2021-09-03 15:35:16 -0400

    refactoring

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -1,27 +1,24 @@
 <template>
-    <main>
-        <h1 class="sent">Message sent!</h1>
-        <p class="thank">Thanks for contacting me! <NuxtLink to="/">Return to the home page</NuxtLink>.</p>
-    </main>
+  <main>
+    <h1 class="sent">Message sent!</h1>
+    <p class="thank">Thanks for contacting me! <NuxtLink to="/">Return to the home page</NuxtLink>.</p>
+  </main>
 </template>
-
-<script>
-</script>
 
 <style scoped>
 .sent {
-    font-size: 6rem;
-    text-align: center;
-    color: var(--heading);
+  font-size: 6rem;
+  text-align: center;
+  color: var(--heading);
 }
 
 .thank {
-    text-align: center;
+  text-align: center;
 }
 
 @media screen and (max-width: 768px) {
-    .sent {
-        font-size: 4rem;
-    }
+  .sent {
+    font-size: 4rem;
+  }
 }
 </style>
