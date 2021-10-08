4ce6e56397dca1814dba6b7c5fc546b77a34f69e
ad <fullmetalbrackets@gmail.com>
2021-10-07 20:45:02 -0400

    updated nav link from /blog to /article

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <span id="logo"><NuxtLink to="/">Ariel Diaz</NuxtLink></span>
     <nav>
       <div><NuxtLink to="/about" class=".nav2">About</NuxtLink></div>
-      <div id="edge"><NuxtLink to="/blog">Blog</NuxtLink></div>
+      <div id="edge"><NuxtLink to="/articles">Blog</NuxtLink></div>
     </nav>
   </header>
 </template>
