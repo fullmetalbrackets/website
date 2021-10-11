7e767cd8e8cf41517672cfe41ee262400670d09d
ad <fullmetalbrackets@gmail.com>
2021-10-11 17:57:36 -0400

    add line-highlight plugin

--- plugins/prism.js
+++ plugins/prism.js
@@ -10,6 +10,8 @@ import "prismjs/plugins/toolbar/prism-toolbar.css";
 // Include some plugins:
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 import "prismjs/plugins/highlight-keywords/prism-highlight-keywords";
+import "prismjs/plugins/line-highlight/prism-line-highlight";
+import "prismjs/plugins/line-highlight/prism-line-highlight.css";
 import "prismjs/plugins/show-language/prism-show-language";
 import "prismjs/plugins/autoloader/prism-autoloader";
 import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
