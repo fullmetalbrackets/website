f3f19c25cdbfb044b6029dcb00647a06a6aee901
ad <fullmetalbrackets@gmail.com>
2021-10-18 01:49:34 -0400

    importing custom prismjs theme for code blocks

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,7 +1,7 @@
 import Prism from "prismjs";
 
 // Include theme:
-import "prismjs/themes/prism-okaidia.css";
+import "prismjs/themes/prism-vaporwave.css";
 
 // Include toolbar plugin:
 import "prismjs/plugins/toolbar/prism-toolbar";
