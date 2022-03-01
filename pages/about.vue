113602fde8b73cd78a3f321476fbc7cac60f406f
Ariel <fullmetalbrackets@gmail.com>
2022-02-28 23:11:44 -0500

    remove redundant text

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
       I'm Ariel, learning web development and living life in sunny/rainy (depending on the time of year) South Florida.
     </p>
     <p>
-      This is my personal website, made with Nuxt.js as a way to learn the framework. Feel free to check out <a href="https://github.com/fullmetalbrackets/website" target="_blank" rel="noopener">this site's source code</a>. Eventually this site will have a portfolio of projects I've worked on, but for now feel free to read my blog -- really just a dump of documentation I've made for myself as I've learned web development, Linux, the command line, etc. If you want to reach out, please use the <Nuxt-Link to="#contact">contact form</Nuxt-Link>. Thanks for visiting!
+      This is my personal website, made with Nuxt.js as a way to learn the framework. Feel free to check out <a href="https://github.com/fullmetalbrackets/website" target="_blank" rel="noopener">this site's source code</a>. Eventually this site will have a portfolio of projects I've worked on, but for now feel free to read my blog -- really just a dump of documentation I've made for myself as I've learned web development, Linux, the command line, etc. Thanks for visiting!
     </p>
     <div id='contact'/>
     <p>
